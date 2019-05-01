163e7cbddde6b7929a995975614f5c9332e9db7b
Maninder97 <42012940+Maninder97@users.noreply.github.com>
2019-05-02 11:45:10 +1200

    Add files via upload
    Method in main class added to show the depot address, customer address and items.
    The values in the constructor for the list has been changed

--- Packer.java
+++ Packer.java
@@ -25,7 +25,36 @@ public class Packer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+          Address depotAddress = new Address("23 Good Luck St", "Blue View", "Sandy Shores", "H337", new Coordinates(138, 995));
+        Depot depot = new Depot("Main Depot", depotAddress);
+        System.out.println("\nDEPOT:");
+        System.out.println(depot);
         
+        Address customerAddress1 = new Address("67 Torch Rd", "Treeline", "Mt High", "T799", new Coordinates(1102, 87));
+        Address customerAddress2 = new Address("88 Camp Mine St", "Ridgeway", "Lowe Valley", "I998", new Coordinates(100, 34));
+        Customer customer = new Customer("Andy Bravo", customerAddress1);
+        customer.addAddress(customerAddress2);
+        System.out.println("\nCUSTOMER");
+        System.out.println(customer);
+
+        Manifest manifest = new Manifest();
+        manifest.addProduct(new Product("Hammer", 3, false, false), 1);
+        manifest.addProduct(new Product("Nails", 1, false, false), 12);
+        manifest.addProduct(new Product("Ladder", 15, false, false), 2);
+        manifest.addProduct(new Product("Saw", 5, false, false), 1);
+        manifest.addProduct(new Product("Light Bulbs", 1, false, true), 20);
+        manifest.addProduct(new Product("Weedkiller", 2, true, false), 1);
+
+        System.out.println("\nMANIFEST (to be packed):");
+        System.out.println(manifest);
+
+        System.out.println("\nPACKING:");
+        List<Box> done = Packer.packProducts(customer, depot, manifest);
+        
+        // Results
+        for (Box b : done) {
+            System.out.println(b);
+        }
         
     }
      public static List<Box> packProducts(Customer c, Depot d, Manifest m) {
@@ -33,8 +62,8 @@ public class Packer {
         Box b = null;
         while (!m.isEmpty()) { // repeat until all items are packed
             if (b == null) {
-              
-                b = new Box(d,c);
+                // b= new box(d,c) changed to b = new Box(c,d)
+                b = new Box(c,d);
             }
             Product prodToAdd = m.getHeaviestUnder(b.remainingCapacity());
             if (prodToAdd == null) {
