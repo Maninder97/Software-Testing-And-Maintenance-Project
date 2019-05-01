f24175ee23ec3e1fb6e32fe967086caf62bf4873
Maninder97 <42012940+Maninder97@users.noreply.github.com>
2019-05-02 11:36:23 +1200

    Add files via upload
    A closing bracket missing at the end

---
+++ Manifest.java
@@ -0,0 +1,127 @@
+package packer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author 90051500
+ */
+public class Manifest {
+    
+    // This tracks the quantity if each product in the manifest
+    public Map<Product, Integer> quantities;
+    // This keeps a list of all products ordered by weight
+    public Set<Product> byWeight;
+
+    public Manifest() {
+        quantities = new HashMap<>();
+        byWeight = new TreeSet<>(new ProductWeightComparator());
+    }
+    
+    public void addProduct(Product p) {
+        addProduct(p,1);
+    }
+    
+    public void addProduct(Product p, int quantity) {
+        if (quantities.containsKey(p)) {
+            quantities.put(p,quantities.get(p)*quantity);
+        }
+        else {
+            quantities.put(p,quantity);
+            if(!byWeight.add(p)) {
+                System.out.println("Couldn't add to Set");
+            }
+        }
+    }
+    
+    public void removeProduct(Product p) {
+        if (quantities.containsKey(p) && quantities.get(p) > 0) {
+            quantities.put(p,quantities.get(p)-1);
+        }
+        if (quantities.get(p) == 0) {
+            quantities.remove(p);
+        }
+        if (quantities.containsKey(p)) {
+            byWeight.remove(p);
+        }
+    }
+    
+    public double getTotalWeight() {
+        double weight = 0;
+        for (Product p : quantities.keySet()) {
+            weight = quantities.get(p) * p.getWeight();
+        }
+        return weight;
+    }
+    
+    public Product getHeaviestUnder(double weight) {
+        for (Product p : byWeight) {
+            if (p.getWeight() <= weight) {
+                return p;
+            }
+        }
+        return null;
+    }
+    
+    public boolean isEmpty() {
+        return byWeight.isEmpty();
+    }
+    
+    public boolean containsProduct(Product p) {
+        return quantities.containsKey(p) && quantities.get(p) > 0;
+    }
+    
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Product p : quantities.keySet()) {
+            result.append(p.getName());
+            result.append(" x ");
+            result.append(quantities.get(p));
+            result.append("\n");
+        }
+        return result.substring(0, result.length()-1);
+    }
+    
+    public boolean hasFragileItems() {
+        for (Product p : quantities.keySet()) {
+            if (p.isFragile()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    /*
+    //Refactoring 1
+    // Method created to label Heavy if the box contains item more than 15Kg.
+    */
+    public boolean hasHeavyItems(){
+        for (Product p : quantities.keySet()) {
+            if (p.isHeavy()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /*
+    //Refactoring 2
+    // Method created to label Hazardious if the box contains hazardious item.
+    */
+    public boolean hasHazardousItems() {
+        for (Product p : quantities.keySet()) {
+            if (p.isHazardous()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    
+
